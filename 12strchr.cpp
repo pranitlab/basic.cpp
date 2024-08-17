d274cf55c65d5b951e2a7e138d5934f96754cce9
Pranit Patil <pranitvp2408@gmail.com>
2024-08-17 22:08:09 +0530

    Add files via upload

---
+++ 12strchr.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+int main()
+{
+    char s1[20]="programming";
+
+    cout<<strchr(s1,'g');
+
+    return 0;
+}
---
+++ 13strrchr.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+int main()
+{
+    char s1[20]="programming";
+
+    cout<<strrchr(s1,'a');
+
+    return 0;
+}
